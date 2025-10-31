02dc8381c1054d62814b509fca98af0f22b982dd
cjang3285 <cjang6199@gmail.com>
2025-10-31 16:55:58 +0900

    error found and fixed: in "add a test" tutorial

---
+++ go-tutorial/create-a-Go-module/greetings/greetings_test.go
@@ -0,0 +1,26 @@
+package greetings
+
+import (
+	"testing"
+	"regexp"
+)
+
+// TestHelloName calls greetings.Hello with a name, checking
+// for a valid return value.
+func TestHelloName(t *testing.T){
+	name := "Jang chanwook"
+	want := regexp.MustCompile("\\b"+name+"\\b") // error fixed 1 :'\b' -> "\\b"
+	msg, err := Hello("Jang chanwook")
+	if !want.MatchString(msg) || err != nil {
+		t.Errorf("Hello(\"Jang chanwook\") = %q, %v, want match for %#q, nil", msg, err, want)
+	}
+}
+
+//TestHelloEmpty calls greetings.Hello with empty string,
+//checking for an error.
+func TestHelloEmpty(t *testing.T){
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Errorf("Hello(\"\") = %q, %v, want \"\", error", msg, err) //error fixed 2 : double quote(") between double quotes("") needs to be written as \"
+	}
+}
